28418068b8091405b5bab19981a5b15e49e18488
steven1303 <58171785+steven1303@users.noreply.github.com>
2021-02-16 16:54:47 +0700

    Partial pada receipt disable dl sementara

--- app/Http/Controllers/Admin/ReceiptController.php
+++ app/Http/Controllers/Admin/ReceiptController.php
@@ -306,12 +306,13 @@ class ReceiptController extends SettingAjaxController
         $movement = $this->rec_movement($data->receipt_detail);
 
         $po_stock = PoStock::findOrFail($data->id_po_stock);
-        $total_terima = $data->po_stock->po_stock_detail->sum('rec_qty') + $data->receipt_detail->sum('terima');
-        if($data->po_stock->po_stock_detail->sum('qty') == $total_terima){
-            $po_stock->po_status = 5;
-        }else{
-            $po_stock->po_status = 4;
-        }
+        // $total_terima = $data->po_stock->po_stock_detail->sum('rec_qty') + $data->receipt_detail->sum('terima');
+        // if($data->po_stock->po_stock_detail->sum('qty') == $total_terima){
+        //     $po_stock->po_status = 5;
+        // }else{
+        //     $po_stock->po_status = 4;
+        // }
+        $po_stock->po_status = 5;
         $po_stock->update();
 
         $data->update();
