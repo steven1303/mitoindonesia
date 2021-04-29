f6294f09b27f40ed4860213f871fbcfc138dc225
steven1303 <58171785+steven1303@users.noreply.github.com>
2021-04-29 19:26:36 +0700

    revisi po stock

--- database/migrations/2021_04_14_162530_create_transfer_receipts_table.php
+++ database/migrations/2021_04_14_162530_create_transfer_receipts_table.php
@@ -15,8 +15,70 @@ class CreateTransferReceiptsTable extends Migration
     {
         Schema::create('transfer_receipts', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('id_branch');
+            $table->string('receipt_transfer_no')->unique();
+            $table->bigInteger('from_branch');
+            $table->dateTime('receipt_transfer_date');
+            $table->integer('receipt_transfer_status');
+            $table->string('user_name');
+            $table->bigInteger('user_id');
+            $table->dateTime('receipt_transfer_open')->nullable();
+            $table->dateTime('receipt_transfer_print')->nullable();
+            $table->timestamps();
             $table->timestamps();
         });
+
+        DB::table('permissions')->insert([
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-view',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-store',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-update',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-delete',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-open',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-approve',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+            [
+                'for' => 'Receipt Transfer',
+                'name' => 'receipt-transfer-print',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+                'stat' => 1,
+            ],
+        ]);
     }
 
     /**
