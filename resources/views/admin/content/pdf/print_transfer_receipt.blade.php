48e14f8f5b2b69502a097631b52ea02538308f12
ABASSUSILO\User <abassusilo.21@gmail.com>
2021-05-10 22:56:34 +0700

    print transfer and receipt transfer

--- resources/views/admin/content/pdf/print_transfer_receipt.blade.php
+++ resources/views/admin/content/pdf/print_transfer_receipt.blade.php
@@ -1,1 +1,110 @@
-Print Transfer Receipt
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>Document</title>
+      </head>
+      <body style="border-style: groove;" width="1200px" >
+      <table width="95%" style="margin: auto; border-collapse: collapse;" >
+        <tbody>
+           <tr colspan="3">
+                  <td colspan="3">
+                      <img src="{{asset('img/PT_Mito_png.png')}}" width="120px" style="float:left">
+                  </td>
+                  <td style="height: 45px;"></td>
+                  <td style="text-align: right;"></td>
+            </tr>
+            <tr>
+              <td colspan="6" style="font-size: 20px; font-weight: bold; text-align: center;">RECEIPT TRANSFER</td>
+            </tr>
+        </tbody>
+    </table>
+    <table width="95%" style="margin: auto; border-collapse: collapse;">
+        </tbody>
+            <tr>
+                <td style="height: 20px;" width="7%"></td>
+                <td style="height: 20px;" width="23%"></td>
+                <td style="height: 20px;" width="30%"></td>
+                <td style="height: 20px;" width="10%"></td>
+                <td style="height: 20px;" width="15%"></td>
+                <td style="height: 20px;" width="15%"></td>
+
+            </tr>
+            <tr style="border: 1px solid black; text-align: center;">
+                <td style="border: 1px solid black;">No.</td>
+                <td style="border: 1px solid black;">Kode Stock</td>
+                <td style="border: 1px solid black;">Deskripsi</td>
+                <td style="border: 1px solid black;">Qty Transfer</td>
+                <td colspan="2" style="border: 1px solid black;">Keterangan</td>
+            </tr>
+         
+            <tr style="border: 1px solid black; height: 20px;">
+                <td style="border: 1px solid black; height: 20px;"></td>
+                <td style="border: 1px solid black; height: 20px;"></td>
+                <td style="border: 1px solid black; height: 20px;"></td>
+                <td style="border: 1px solid black; height: 20px;"></td>
+                <td style="border: 1px solid black; height: 20px;"></td>
+                <td style="border: 1px solid black; height: 20px;"></td>
+            </tr>
+              <tr>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+            </tr>
+            <tr>
+                <td colspan="3">Note : …..........................................................</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+                <td style="height: 20px;"></td>
+            </tr>
+            <tr>
+                <td colspan="2" border="1">Di Buat Oleh :</td>
+                <td>Di Setujui Oleh :</td>
+                <td colspan="2" >Total Qty Order        :</td>
+                <td style="text-align: left"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td colspan="2">Total Qty Terima      :</td>
+                <td style="text-align: left"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td colspan="2">Total Qty BO            :</td>
+                <td style="text-align: left"></td>
+            </tr>
+            <tr>
+                <td colspan="2">….....................</td>
+                <td>.....................</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    </body>
+</html>
