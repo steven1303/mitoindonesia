883723b59483455db30d5028ccf359bc02741748
ABASSUSILO\User <abassusilo.21@gmail.com>
2021-08-12 11:59:54 +0700

    update form PO non Stock

--- resources/views/admin/content/pdf/print_po_non_stock_medan.blade.php
+++ resources/views/admin/content/pdf/print_po_non_stock_medan.blade.php
@@ -120,8 +120,8 @@
                     <td style="border: 1px solid black;">{{ $loop->iteration }}</td>
                     <td style="border: 1px solid black; text-align: left">{{ $detail->product }}</td>
                     <td style="border: 1px solid black;">{{ $detail->spb_detail->qty }} {{ $detail->spb_detail->satuan }}</td>
-                    <td style="border: 1px solid black;">{{ "Rp. ".number_format( ($detail->price),0, ",", ".") }}</td>
-                    <td style="border: 1px solid black;">{{ "Rp. ".number_format( ($detail->spb_detail->qty * $detail->price),0, ",", ".") }}</td>
+                    <td style="border: 1px solid black; text-align: right;">{{ "Rp. ".number_format( ($detail->price),0, ",", ".") }}</td>
+                    <td style="border: 1px solid black; text-align: right;">{{ "Rp. ".number_format( ($detail->spb_detail->qty * $detail->price),0, ",", ".") }}</td>
                 </tr>
             @endforeach
             @for ($i = $po_stock->po_non_stock_detail->count(); $i < 25; $i++)
